f41011c7f2e90b3cd2b3a9a07bd6c81adf0dc694
Dan Murphy <dmurphy@ti.com>
2011-10-11 13:54:34 -0500

    ARM: Update ION code for commonality
    Created a omap4_ion.h so that multiple products can configure
    the same ION support
    
    Change-Id: Id6ac1e07f11dec14a66b8b4078a45c1ed66fe7bc
    Signed-off-by: Dan Murphy <dmurphy@ti.com>
    
    Conflicts:
    	arch/arm/mach-omap2/board-4430sdp.c
    	arch/arm/mach-omap2/board-44xx-tablet.c
    	arch/arm/mach-omap2/board-44xx-tablet.h
    	arch/arm/mach-omap2/board-omap4panda.c
    	include/linux/platform_data/ram_console.h

---
+++ arch/arm/mach-omap2/omap4_ion.h
@@ -0,0 +1,28 @@
+/*
+ * arch/arm/mach-omap2/board-blaze.h
+ *
+ * Copyright (C) 2011 Texas Instruments
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _OMAP4_ION_H
+#define _OMAP4_ION_H
+
+#ifdef CONFIG_ION_OMAP
+void omap_ion_init(void);
+void omap4_register_ion(void);
+#else
+inline void omap_ion_init(void) { return; }
+inline void omap4_register_ion(void) { return; }
+#endif
+
+#endif
