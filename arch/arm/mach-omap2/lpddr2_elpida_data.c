ad6777a7e0e7a40514acf6ae151d1038ef7af31e
Taras Kondratiuk <taras@ti.com>
2011-12-11 20:55:57 -0600

    OMAP4: EMIF: Add 4Gb memory Configuration data
    Elpida EDB8164B3PF is going to be used on OMAP boards.
    Unlike the previous memory this one has two pieces of 4Gb,
    so a new configuration stucture should be added.
    
    Change-Id: I98cf8fd2689c21ef7b076aceaf4ed6f7a1b6b0b1
    Signed-off-by: Taras Kondratiuk <taras@ti.com>

--- arch/arm/mach-omap2/lpddr2_elpida_data.c
+++ arch/arm/mach-omap2/lpddr2_elpida_data.c
@@ -133,3 +133,17 @@ struct lpddr2_device_info lpddr2_elpida_2G_S4_dev = {
 	.density	= LPDDR2_DENSITY_2Gb,
 	.io_width	= LPDDR2_IO_WIDTH_32
 };
+
+struct lpddr2_device_info lpddr2_elpida_4G_S4_dev = {
+	.device_timings = {
+		&lpddr2_elpida_timings_200_mhz,
+		&lpddr2_elpida_timings_333_mhz,
+		&lpddr2_elpida_timings_400_mhz,
+		&lpddr2_elpida_timings_466_mhz,
+	},
+	.min_tck	= &lpddr2_elpida_min_tck,
+	.type		= LPDDR2_TYPE_S4,
+	.density	= LPDDR2_DENSITY_4Gb,
+	.io_width	= LPDDR2_IO_WIDTH_32,
+	.emif_ddr_selfrefresh_cycles = 262144,
+};
