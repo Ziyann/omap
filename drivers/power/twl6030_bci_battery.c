074b312cef33e6afd6accc07f8ebd6d38738dc84
Oleksandr Dmytryshyn <oleksandr.dmytryshyn@ti.com>
2012-04-26 11:21:22 +0300

    MFD: TWL6030: Introduce errata implementation
    This patch adds for the kernel code ability to the errata
    implementation for the TWL6030/TWL6032 chips.
    
    Change-Id: Ieb2329ec3cf1e0300decc2908fba28aa471854e5
    Signed-off-by: Oleksandr Dmytryshyn <oleksandr.dmytryshyn@ti.com>

--- drivers/power/twl6030_bci_battery.c
+++ drivers/power/twl6030_bci_battery.c
@@ -300,6 +300,7 @@ struct twl6030_bci_device_info {
 	struct delayed_work	twl6030_current_avg_work;
 
 	unsigned long		features;
+	unsigned long		errata;
 };
 
 static BLOCKING_NOTIFIER_HEAD(notifier_list);
@@ -2176,6 +2177,7 @@ static int __devinit twl6030_bci_battery_probe(struct platform_device *pdev)
 
 	di->platform_data = pdata;
 	di->features = pdata->features;
+	di->errata = pdata->errata;
 
 	if (pdata->use_eeprom_config &&
 			di->features & TWL6032_SUBCLASS) {
@@ -2228,6 +2230,7 @@ static int __devinit twl6030_bci_battery_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, di);
 
 	wake_lock_init(&chrg_lock, WAKE_LOCK_SUSPEND, "ac_chrg_wake_lock");
+
 	/* settings for temperature sensing */
 	ret = twl6030battery_temp_setup(true);
 	if (ret)
