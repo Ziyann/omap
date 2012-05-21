e70e8d33b3bf52f32e0f4ece4556ff81aad6c5ea
Devaraj Rangasamy <dev@ti.com>
2012-05-22 00:35:05 +0530

    omap: common: add kernel API to query RAM size
    Add kernel api omap_total_ram_size() to query the
    exact size of physical ram available in system.
    Its derived from mem arguments passed to kernel via
    bootargs and is expected to be used by board files and drivers.
    Note: omap_init_ram_size() has to be invoked at the earliest
    during board init to update memory size.
    
    Change-Id: I8511fd66ba51343bc64a58db04994af930cbe709
    Signed-off-by: Devaraj Rangasamy <dev@ti.com>
    
    Conflicts:
    	arch/arm/mach-omap2/board-4430sdp.c
    	arch/arm/mach-omap2/board-44xx-tablet.c
    	arch/arm/mach-omap2/board-omap4panda.c

--- arch/arm/mach-omap2/board-tuna.c
+++ arch/arm/mach-omap2/board-tuna.c
@@ -1341,6 +1341,8 @@ static void __init tuna_map_io(void)
 
 static void __init tuna_reserve(void)
 {
+	omap_init_ram_size();
+	
     omap_ram_console_init(OMAP_RAM_CONSOLE_START_DEFAULT,
             OMAP_RAM_CONSOLE_SIZE_DEFAULT);
 
