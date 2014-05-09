e7c57d7b8a2d4217602d7cc5a65b501e8b7e2012
Israel Cepeda <israel4c@gmail.com>
2014-05-08 21:35:04 -0700

    tuna: power: update function calls
    Update to use functions that have change for kernel 3.4
    
    Signed-off-by: Israel Cepeda <israel4c@gmail.com>

--- arch/arm/mach-omap2/board-tuna-power.c
+++ arch/arm/mach-omap2/board-tuna-power.c
@@ -37,6 +37,7 @@
 #include <plat/omap-pm.h>
 
 #include "board-tuna.h"
+#include "twl-common.h"
 #include "mux.h"
 #include "pm.h"
 
@@ -485,18 +486,6 @@ void __init omap4_tuna_power_init(void)
 				OMAP_PIN_OFF_OUTPUT_HIGH, -1);
 	if (status)
 		pr_err("TPS62361 initialization failed: %d\n", status);
-	/*
-	 * Some Tuna devices have a 4430 chip on a 4460 board, manually
-	 * tweak the power tree to the 4460 style with the TPS regulator.
-	 */
-	if (cpu_is_omap443x()) {
-		/* Disable 4430 mapping */
-		omap_twl_pmic_update("mpu", CHIP_IS_OMAP443X, 0x0);
-		omap_twl_pmic_update("core", CHIP_IS_OMAP443X, 0x0);
-		/* make 4460 map usable for 4430 */
-		omap_twl_pmic_update("core", CHIP_IS_OMAP446X, CHIP_IS_OMAP443X);
-		omap_tps6236x_update("mpu", CHIP_IS_OMAP446X, CHIP_IS_OMAP443X);
-	}
 
 	/* Update temperature data from board type */
 	if (omap4_tuna_get_type() == TUNA_TYPE_TORO) {
@@ -519,7 +508,7 @@ void __init omap4_tuna_power_init(void)
 		 * tstart = 10ms + 5ms = 15ms.
 		 * tshut = 1us (rounded)
 		 */
-		omap_pm_set_osc_lp_time(15000, 1);
+		omap_pm_setup_oscillator(15000, 1);
 	} else {
 		/*
 		 * sample 5 onwards (Toro and Maguro), we use SQ200384:
@@ -529,7 +518,7 @@ void __init omap4_tuna_power_init(void)
 		 * tstart = 10ms + 10ms = 20ms.
 		 * tshut = 1us (rounded)
 		 */
-		omap_pm_set_osc_lp_time(20000, 1);
+		omap_pm_setup_oscillator(20000, 1);
 	}
 
 	omap_mux_init_gpio(charger_gpios[0].gpio, OMAP_PIN_INPUT);
