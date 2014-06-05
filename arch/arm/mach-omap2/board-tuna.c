9e3cc9ce20a16e1620eaa9ea3b3c65eba7e50363
Hashcode <hashcode0f@gmail.com>
2014-06-05 10:13:26 -0700

    tuna: cleanup machine map_io
    - remote duplicate call to omap2_set_globals_443x (called in init_early)
    - removes need for breakout function

--- arch/arm/mach-omap2/board-tuna.c
+++ arch/arm/mach-omap2/board-tuna.c
@@ -1037,12 +1037,6 @@ static void __init tuna_init(void)
 #endif
 }
 
-static void __init tuna_map_io(void)
-{
-	omap2_set_globals_443x();
-	omap44xx_map_common_io();
-}
-
 static void __init tuna_reserve(void)
 {
     omap_ram_console_init(OMAP_RAM_CONSOLE_START_DEFAULT,
@@ -1077,7 +1071,7 @@ MACHINE_START(TUNA, "Tuna")
 	/* Maintainer: Google, Inc */
 	.atag_offset	= 0x100,
 	.reserve	= tuna_reserve,
-	.map_io		= tuna_map_io,
+	.map_io		= omap4_map_io,
 	.init_early	= omap4430_init_early,
 	.init_irq	= gic_init_irq,
 	.handle_irq	= gic_handle_irq,
