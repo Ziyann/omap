9bb8d38de66bc9db4ea6f19702f414c8f4a1ea3e
Hashcode <hashcode0f@gmail.com>
2014-07-10 16:34:15 -0700

    tuna: cleanup duplicate ddr vars

--- arch/arm/mach-omap2/board-tuna-emif.c
+++ arch/arm/mach-omap2/board-tuna-emif.c
@@ -14,67 +14,8 @@
 
 #include <linux/platform_data/emif_plat.h>
 #include "common.h"
+#include "common-board-devices.h"
 #include "board-tuna.h"
-
-struct lpddr2_timings lpddr2_samsung_4G_S4_timings[] = {
-	[0] = {
-		.max_freq	= 400000000,
-		.tRPab		= 21000,
-		.tRCD		= 18000,
-		.tWR		= 15000,
-		.tRAS_min	= 42000,
-		.tRRD		= 10000,
-		.tWTR		= 15000,
-		.tXP		= 15000,
-		.tRTP		= 15000,
-		.tCKESR		= 15000,
-		.tZQCS		= 90000,
-		.tZQCL		= 360000,
-		.tZQinit	= 1000000,
-		.tDQSCK_max	= 11000,
-		.tRAS_max_ns	= 70000,
-		.tFAW		= 50000,
-	},
-	[1] = {
-		.max_freq	= 200000000,
-		.tRPab		= 21000,
-		.tRCD		= 18000,
-		.tWR		= 15000,
-		.tRAS_min	= 42000,
-		.tRRD		= 10000,
-		.tWTR		= 20000,
-		.tXP		= 15000,
-		.tRTP		= 15000,
-		.tCKESR		= 15000,
-		.tZQCS		= 90000,
-		.tZQCL		= 360000,
-		.tZQinit	= 1000000,
-		.tDQSCK_max	= 11000,
-		.tRAS_max_ns	= 70000,
-		.tFAW		= 50000,
-	},
-};
-
-struct lpddr2_min_tck lpddr2_samsung_S4_min_tck = {
-	.tRPab		= 3,
-	.tRCD		= 3,
-	.tWR		= 3,
-	.tRASmin	= 3,
-	.tRRD		= 2,
-	.tWTR		= 2,
-	.tXP		= 2,
-	.tRTP		= 2,
-	.tCKE		= 3,
-	.tCKESR		= 3,
-	.tFAW		= 8
-};
-
-struct ddr_device_info lpddr2_samsung_4G_S4_info = {
-	.type		= DDR_TYPE_LPDDR2_S4,
-	.density	= DDR_DENSITY_4Gb,
-	.io_width	= DDR_IO_WIDTH_32,
-	.manufacturer = "Samsung",
-};
 
 static struct __devinitdata emif_custom_configs custom_configs = {
 	.mask	= EMIF_CUSTOM_CONFIG_LPMODE,
