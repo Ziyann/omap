6898c84a68974e7a25394ece4ead5d13d0ac9584
Hashcode <hashcode0f@gmail.com>
2014-07-17 15:24:18 -0700

    Revert "ARM: OMAP44xx: Remove dss_fck clock"
    This reverts commit 10a1e29f332c9166905cf946bbeb17a6d3dac761.

--- arch/arm/mach-omap2/omap_hwmod_44xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_44xx_data.c
@@ -1963,7 +1963,7 @@ static struct omap_hwmod_ocp_if *omap44xx_dss_rfbi_slaves[] = {
 };
 
 static struct omap_hwmod_opt_clk dss_rfbi_opt_clks[] = {
-	{ .role = "ick", .clk = "l3_div_ck" },
+	{ .role = "ick", .clk = "dss_fck" },
 };
 
 static struct omap_hwmod omap44xx_dss_rfbi_hwmod = {
