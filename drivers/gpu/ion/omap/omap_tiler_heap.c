6c55945c6cb0d00cf02683ab503bfdf9282f4a98
Hashcode <hashcode0f@gmail.com>
2015-02-17 20:43:08 -0800

    Revert "omap: ion: fix #ifdef check"
    This reverts commit e1db565192d8bdf71beab0fc8f8e99a00321491d.

--- drivers/gpu/ion/omap/omap_tiler_heap.c
+++ drivers/gpu/ion/omap/omap_tiler_heap.c
@@ -500,7 +500,7 @@ struct ion_heap *omap_tiler_heap_create(struct ion_platform_heap *data)
 	heap->heap.name = data->name;
 	heap->heap.id = data->id;
 
-#ifdef CONFIG_ION_OMAP_USE_DYNAMIC_PAGES
+#if CONFIG_ION_OMAP_USE_DYNAMIC_PAGES
 	use_dynamic_pages = true;
 #else
 	if (omap_total_ram_size() <= SZ_512M)
