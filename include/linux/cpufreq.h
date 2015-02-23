ec9f980dbab9634267cd577c03681814353d0520
Noeljunior <liamgliam@gmail.com>
2015-02-23 10:48:39 +0000

    cpufreq: added Sakuractive governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -372,6 +372,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAKURACTIVE)
+extern struct cpufreq_governor cpufreq_gov_sakuractive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sakuractive)
 #endif
 
 
