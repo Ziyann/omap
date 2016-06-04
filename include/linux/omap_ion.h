0cd4fdbf15ff6fe2a33be1da0d7d546c116adbde
Ziyan <jaraidaniel@gmail.com>
2016-06-04 10:22:52 +0200

    Revert "ion: add support for tiler reservation heap"
    This reverts commit 257072ea273263eff410fd3e429add5c89a46d11.

--- include/linux/omap_ion.h
+++ include/linux/omap_ion.h
@@ -66,7 +66,6 @@ extern struct ion_device *omap_ion_device;
 /* additional heaps used only on omap */
 enum {
 	OMAP_ION_HEAP_TYPE_TILER = ION_HEAP_TYPE_CUSTOM + 1,
-	OMAP_ION_HEAP_TYPE_TILER_RESERVATION,
 };
 
 #define OMAP_ION_HEAP_TILER_MASK (1 << OMAP_ION_HEAP_TYPE_TILER)
@@ -95,7 +94,6 @@ enum {
 	OMAP_ION_HEAP_TILER,
 	OMAP_ION_HEAP_SECURE_INPUT,
 	OMAP_ION_HEAP_NONSECURE_TILER,
-	OMAP_ION_HEAP_TILER_RESERVATION,
 };
 
 #endif /* _LINUX_ION_H */
